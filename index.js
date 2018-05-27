75d9817febd6e84e1a394082ca4fe8a6c9abd894
vasyl-n <vasyl.nrk@gmail.com>
2018-05-26 20:08:34 -0700

    redirects to a service and sends resp back

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const Koa = require('koa');
 const Router = require('koa-router')
+var request = require('request-promise');
 
 const app = new Koa()
 const router = new Router()
 
 router.get('/', (ctx) => {
-  ctx.body = 'Hello, from GET';
+  let response = request({
+    method: 'GET',
+    url: 'http://127.0.0.1:3000/',
+  })
+  ctx.body = response;
 });
 
 
