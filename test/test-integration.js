17e6c5331007b0c708015a2ad5954c3d3f9b6f21
vasyl-n <vasyl.nrk@gmail.com>
2018-05-28 17:40:08 -0700

    adds sinon to the tests

---
+++ test/test-integration.js
@@ -0,0 +1,18 @@
+var chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+const request = require('supertest');
+const app = require('../index');
+ 
+xdescribe('GET /', function() {
+  it('respond with 200 status code', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
+
+after('close server connection after tests', function() {
+  app.close()
+});
--- test/test.js
+++ test/test.js
@@ -2,17 +2,45 @@ var chai = require('chai')
   , expect = chai.expect
   , should = chai.should();
 const request = require('supertest');
-const app = require('../index')
-  
- 
-describe('GET /', function() {
-  it('respond with 200 status code', function(done) {
-    request(app)
-      .get('/')
-      .expect(200, done);
+const app = require('../index');
+const sinon = require('sinon');
+
+
+describe('when stubbed', () => {
+  const request = require('request');
+  before(() => {
+    this.get = sinon.stub(request, 'get');
+    this.responseObject = {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json'
+      }
+    };
+    this.responseBody = {
+      status: 'success',
+      data: ['test'
+      ]
+    };
+  });
+
+  afterEach(() => {
+    request.get.restore();
+  });
+  describe('GET /', () => {
+
+    it('should get /', (done) => {
+      this.get.yields(null, this.responseObject, JSON.stringify(this.responseBody));
+      request.get(`/`, (err, res, body) => {
+        res.statusCode.should.eql(200);
+        res.headers['content-type'].should.contain('application/json');
+        body = JSON.parse(body);
+        body.status.should.eql('success');
+        body.data.length.should.eql(1);
+        done();
+      });
+    });
   });
 });
-
 
 after('close server connection after tests', function() {
   app.close()
