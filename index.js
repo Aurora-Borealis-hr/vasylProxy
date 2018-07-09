3a581f821fc1d9f06479d10704fdad0f23bc2f17
vasyl-n <vasyl.nrk@gmail.com>
2018-07-08 21:39:56 -0700

    adds commented elastic search to index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express')
 var request = require('request-promise')
 var app = express()
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
 var API_URL = require('./config').API_URL
+var client = require('./elastic')
 
 app.get('/*', function (req, res) {
+  // client.index({  
+  //   index: 'web-requests',
+  //   type: '_doc',
+  //   body: {
+  //     "userId": req.cookies.userId,
+  //     "date": Date.now(),
+  //     "user-agent": req.headers['user-agent'],
+  //     "url": req.path
+  //   }
+  // },function(err,resp,status) {
+  //     // console.log(resp);
+  // });
     var response = request({
     method: 'GET',
     url: `${API_URL}${req.path}`,
